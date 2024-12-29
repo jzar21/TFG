19faab178e46de44601541e6c7d500203f9a192f
jzar21 <juanfmontilla21@gmail.com>
2024-12-29 17:49:24 +0100

    cambio de la arquitectura

--- codigo/create_model.py
+++ codigo/create_model.py
@@ -1,33 +1,12 @@
-import torch
+from resnets_3d.models.resnet import generate_model
 import torch.nn as nn
-from MedicalNet.models.resnet import ResNet, Bottleneck
 
 
-class ResNet3D_Regresion(ResNet):
-    def __init__(self,
-                 layers,
-                 sample_input_D,
-                 sample_input_H,
-                 sample_input_W,
-                 num_output,
-                 shortcut_type='B',
-                 no_cuda=False):
-        self.inplanes = sample_input_D
-        self.no_cuda = no_cuda
-        super(ResNet3D_Regresion, self).__init__(Bottleneck, layers, sample_input_D,
-                                                 sample_input_H,
-                                                 sample_input_W,
-                                                 num_seg_classes=1,
-                                                 shortcut_type=shortcut_type,
-                                                 no_cuda=no_cuda)
-
-        self.poll = nn.AdaptiveAvgPool3d(
-            (1, self.inplanes, Bottleneck.expansion))
-        self.fc = nn.Linear(self.inplanes * Bottleneck.expansion, num_output)
+class ResNet3D_Regresion(nn.Module):
+    def __init__(self, model_depth: int = 18, n_input_channels: int = 1,
+                 n_classes: int = 1):
+        self.model = generate_model(
+            model_depth, n_input_channels=n_input_channels, n_classes=n_classes)
 
     def forward(self, x):
-        x = super(ResNet3D_Regresion, self).forward(x)
-        x = self.poll(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
-        return x
+        return self.model(x)
